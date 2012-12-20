fbd76d85a30ecd10c137f6f47256b006464da8ae
bossiel <bossiel@yahoo.fr>
2012-12-20 07:03:50 +0000

    API version 1.1.0 - Adds support for Presence publication and subscription - Fix issue 56, 57 and 59

--- docs/html/search/all_67.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['get_5fid',['get_id',['../classtsip__session.html#a496a366773048acf750e25ca337ab36b',1,'tsip_session']]],
-  ['get_5fstack',['get_stack',['../classtsip__session.html#ae58e6e54a32cfe100035ff0784e704d7',1,'tsip_session']]]
-];
