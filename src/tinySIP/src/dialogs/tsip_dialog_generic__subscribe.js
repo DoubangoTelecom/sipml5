97089cd4cb86677c855a49d6445c1321bdade502
Arkady Smirnov <arkady.smirnov@uvoteam.com>
2016-04-13 20:41:41 +0700

    FIX: the SUBSCRIBE timer is updated each time it receives a NOTIFY

--- src/tinySIP/src/dialogs/tsip_dialog_generic__subscribe.js
+++ src/tinySIP/src/dialogs/tsip_dialog_generic__subscribe.js
@@ -70,8 +70,14 @@ function tsip_dialog_generic_Any_2_Any_X_iNotify(ao_args) {
      }
 
      // update timeout using expires from subscription state (e.g. 'Subscription-State: pending;expires=200')
-     o_dialog.i_timerRefresh = o_dialog.get_newdelay(o_request);
-	 o_dialog.timer_schedule('generic', 'Refresh');
+	 var i_newdelay = o_dialog.get_newdelay(o_request);
+
+     // The notifier MAY use this mechanism to shorten a subscription;
+     // however, this mechanism MUST NOT be used to lengthen a subscription
+     if ( i_newdelay < o_dialog.i_timerRefresh ) {
+        o_dialog.i_timerRefresh = i_newdelay;
+        o_dialog.timer_schedule('generic', 'Refresh');
+     }
 
      // alert user
      o_dialog.signal_i(tsip_event_code_e.DIALOG_REQUEST_INCOMING, 'Incoming NOTIFY', o_request);
